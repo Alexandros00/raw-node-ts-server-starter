150d7f2a52439e40168a5431787572e55d447a1e
Alexandros Karalis <karalisalexandros@gmail.com>
2025-05-26 18:56:21 +0300

    feat(get): handle persistent GET requests

--- src/routes/products/get.ts
+++ src/routes/products/get.ts
@@ -1,22 +1,29 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { getPathParts, sendJsonResponse } from "../../utils/httpUtils.js";
-import { products } from "../../product/interfaces/product.interface.js";
+import productService from "../../services/product/ProductService.js";
+import { HttpError } from "../HttpError.js";
 
 export function handleProductsGetRequest(req: IncomingMessage, res: ServerResponse) {
   const { resource, stringId } = getPathParts(req.url ?? "");
   const id = Number(stringId);
 
-  if (resource === "products" && !stringId) {
-    sendJsonResponse(res, 200, products);
-  } else if (resource === "products" && Number.isInteger(id)) {
-    const product = products.find((p) => p.id === id);
+  const isCollectionRequest = resource === "products" && !stringId;
+  const isItemRequest = resource === "products" && Number.isInteger(id);
 
-    if (product) {
-      sendJsonResponse(res, 200, product);
+  try {
+    if (isCollectionRequest) {
+      sendJsonResponse(res, 200, productService.getAllProducts());
+    } else if (isItemRequest) {
+      sendJsonResponse(res, 200, productService.getProductById(id));
     } else {
-      sendJsonResponse(res, 404, { error: "Product not found" });
+      sendJsonResponse(res, 400, { error: "Bad request" });
     }
-  } else {
-    sendJsonResponse(res, 400, { error: "Bad request" });
+  } catch (error) {
+    if (error instanceof HttpError) {
+      sendJsonResponse(res, error.status, { error: error.message });
+    } else {
+      console.error("Unhandled error:", error);
+      sendJsonResponse(res, 500, { error: "Internal server error" });
+    }
   }
 }
