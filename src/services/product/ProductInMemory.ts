cdb0f2ef6badad28d376088cb778bd3c15476c3f
Alexandros Karalis <karalisalexandros@gmail.com>
2025-05-26 18:54:15 +0300

    feat(class): add ProductInMemory class to handle in memory products

---
+++ src/services/product/ProductInMemory.ts
@@ -0,0 +1,33 @@
+import { Product } from "../../product/interfaces/product.interface";
+
+export class ProductInMemory {
+  private _products: Product[] = [];
+
+  protected getAllFromMemory(): Product[] {
+    return this._products;
+  }
+
+  protected getById(id: number): Product | undefined {
+    if (id < 0) throw new Error("Invalid ID");
+
+    return this._products.find((p) => p.id === id);
+  }
+
+  protected saveItem(product: Product): Product {
+    const newProduct: Product = {
+      id: this._products.length + 1,
+      ...product,
+    };
+    this._products.push(newProduct);
+    return newProduct;
+  }
+
+  protected updateItem(index: number, product: Product): Product {
+    this._products[index] = product;
+    return this._products[index];
+  }
+
+  protected saveAllToMemory(products: Product[]): void {
+    this._products = products;
+  }
+}
